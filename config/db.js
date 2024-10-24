bd9efa391ec056bdea8fbbb8644a04e67a0b2af3
Pritiranjan Pradhan <128985988+prpradhan13@users.noreply.github.com>
2024-10-25 01:36:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const db = await mongoose.connect(`${process.env.MONGO_URI}/project-zero`);
-        console.log(`Connecting to ${db.connection.host}`);
+        console.log(`Connecting to DB`);
     } catch (error) {
         console.log(error);
         process.exit(1);        
